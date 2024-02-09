bc501418e626768a8d72739d087d2a0d6ba9b412
Phakpoom Ittirattanakomon <phakpoomittirattanakomon@Phakpooms-MacBook-Pro.local>
2024-02-10 01:23:00 +0700

    first commit

---
+++ src/main/java/com/example/newton/model/AppUser.java
@@ -0,0 +1,33 @@
+package com.example.newton.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class AppUser {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String role;
+
+  public AppUser(String username, String password, String role) {
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
+}
